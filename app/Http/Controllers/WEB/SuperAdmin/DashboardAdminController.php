1f8f04a681d99752f460f1900874fbf0438ad730
anamoe <anam45188@gmail.com>
2022-07-22 16:22:39 +0700

    public/ to / and asset / to public/

--- app/Http/Controllers/WEB/SuperAdmin/DashboardAdminController.php
+++ app/Http/Controllers/WEB/SuperAdmin/DashboardAdminController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\WEB\SuperAdmin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Anak;
 use App\Models\Kecamatan;
+use App\Models\Orangtua;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -34,6 +36,87 @@ class DashboardAdminController extends Controller
         ->select('posyandu.*','desa_kelurahan.nama')
         ->where('desa_kelurahan_id',$id)
         ->get();
+       
+
+        foreach ($data_pos as $key => $value) {
+            $data_pos[$key]->orangtua =
+                Orangtua::where('posyandu_id', $value->id)->with(
+            ['anaks.penimbangans'
+            => function ($q)  {
+                $q->select("*", DB::raw('YEAR(created_at) year, MONTH(created_at) bulan'));
+            }
+            ]
+        )->get();
+
+        foreach ($data_pos[$key]->orangtua as $Orangtua) {
+        
+            if (!$Orangtua->anaks->isEmpty()) {
+                foreach ($Orangtua->anaks as $anak) {
+                    // $umuranak = $this->cekumur($anak->tanggal_lahir);
+                    // if ($umuranak > 0 && $umuranak <= 24) {
+                    
+                        if (!$anak->penimbangans->isEmpty()) {
+                            foreach ($anak->penimbangans as $penimbangan) {
+                                // return $penimbangan->orderBy('id','desc')->first();
+
+                                $p = $penimbangan->select(
+                                    DB::raw('count(id) as `jumlah`'),
+                                    )->where('status_bb_pb','Gizi Buruk')->limit(1)
+                                   
+                                    
+                                    // DB::raw("tanggal_kursus","id_jam"),
+                                    // DB::raw("id_jam as jam_kursus" ),)
+                                    // ->groupBy(['tanggal_kursus','id_jam'])->orderBy('tanggal_kursus')
+                                    ->get();
+                                    return $p;
+                              
+                            }
+                        // }
+                    
+                    }
+                }
+            }
+        }
+
+
+
+        
+
+          
+
+
+        }
+        return $data_pos;
+
+    
+        // $ortu = Orangtua::where('posyandu_id',$id)->get();
+        // foreach ($ortu as $key => $value) {
+        //     // $ortu[$key]->jumlah_anak = DB::table('anak')
+        //     // ->where('orangtua_id',$value->id)
+        //     // ->count();
+        //     $anak = Anak::where('orangtua_id',$value->id)->get();
+        //     // $isi_anak = $anak;
+          
+        // }
+        // return $anak;
+
+        // // return $ortu->anak;
+
+        // return $data_pos;
+
+        // $ortu = Orangtua::where('posyandu_id', $data_pos->id)->with(
+        //     ['anaks.penimbangans'
+        //     => function ($q)  {
+        //         $q->select("*", DB::raw('YEAR(created_at) year, MONTH(created_at) bulan'));
+        //         // $q->orderBy('id', 'desc')->first();
+              
+              
+        //     }]
+        // )->get();
+
+     
+
+        // return $ortu;
 
         return view('admin.rekap_posyandu', compact('data_pos'));
     }
