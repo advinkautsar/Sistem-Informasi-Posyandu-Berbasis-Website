f8c02a7da0f007b254949a0a442050383b5ba972
anamoe <anam45188@gmail.com>
2022-07-23 01:17:57 +0700

    dashboard admin sehat dan sakit count

--- app/Http/Controllers/WEB/SuperAdmin/DashboardAdminController.php
+++ app/Http/Controllers/WEB/SuperAdmin/DashboardAdminController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\WEB\SuperAdmin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Anak;
+use App\Models\Desa_kelurahan;
 use App\Models\Kecamatan;
 use App\Models\Orangtua;
+use App\Models\Posyandu;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -46,9 +48,32 @@ class DashboardAdminController extends Controller
 
     public function rekap_desa($id)
     {
-        $data_desa = DB::table('desa_kelurahan')
-        ->where('kecamatan_id',$id)
+        $data_desa = Desa_kelurahan::where('kecamatan_id', $id)->get();
+        // ->where('kecamatan_id',$id)
+        // ->get();
+
+        $data = Kecamatan::
+        leftjoin('desa_kelurahan', 'desa_kelurahan.kecamatan_id', 'kecamatan.id')
+        ->join('posyandu', 'posyandu.desa_kelurahan_id', 'desa_kelurahan.id')
+        ->join('orangtua', 'orangtua.posyandu_id', 'posyandu.id')
+        ->join('anak', 'anak.orangtua_id', 'orangtua.id')
+        ->join('penimbangan', 'penimbangan.nik_anak', 'anak.nik_anak')
         ->get();
+
+        foreach($data_desa as $desa)
+        {
+           $desa['jumlah_sehat'] = $data
+           ->where('nama', $desa->nama)
+           ->where('status_bb_tb','Normal')
+           ->where('status_bb_pb','Normal')
+           ->count();
+
+           $desa['jumlah_sakit'] = $data
+           ->where('nama', $desa->nama)
+           ->where('status_bb_tb','Gizi buruk')
+           ->where('status_bb_pb','Gizi buruk')
+           ->count();
+        }
 
         // return $data_desa;
 
@@ -57,92 +82,38 @@ class DashboardAdminController extends Controller
 
     public function rekap_posyandu($id)
     {
-        $data_pos = DB::table('posyandu')
-        ->join('desa_kelurahan','posyandu.desa_kelurahan_id','desa_kelurahan.id')
+        $data_pos = Posyandu::
+        join('desa_kelurahan','posyandu.desa_kelurahan_id','desa_kelurahan.id')
         ->select('posyandu.*','desa_kelurahan.nama')
         ->where('desa_kelurahan_id',$id)
         ->get();
+
+        $data = Kecamatan::
+        leftjoin('desa_kelurahan', 'desa_kelurahan.kecamatan_id', 'kecamatan.id')
+        ->join('posyandu', 'posyandu.desa_kelurahan_id', 'desa_kelurahan.id')
+        ->join('orangtua', 'orangtua.posyandu_id', 'posyandu.id')
+        ->join('anak', 'anak.orangtua_id', 'orangtua.id')
+        ->join('penimbangan', 'penimbangan.nik_anak', 'anak.nik_anak')
+        ->get();
+
+        foreach($data_pos as $pos)
+        {
+           $pos['jumlah_sehat'] = $data
+           ->where('nama_posyandu', $pos->nama_posyandu)
+           ->where('status_bb_tb','Normal')
+           ->where('status_bb_pb','Normal')
+           ->count();
+
+           $pos['jumlah_sakit'] = $data
+           ->where('nama_posyandu', $pos->nama_posyandu)
+           ->where('status_bb_tb','Gizi buruk')
+           ->where('status_bb_pb','Gizi buruk')
+           ->count();
+        }
        
-
-        foreach ($data_pos as $key => $value) {
-            $data_pos[$key]->orangtua =
-                Orangtua::where('posyandu_id', $value->id)->with(
-            ['anaks.penimbangans'
-            => function ($q)  {
-                $q->select("*", DB::raw('YEAR(created_at) year, MONTH(created_at) bulan'));
-            }
-            ]
-        )->get();
-
-        foreach ($data_pos[$key]->orangtua as $Orangtua) {
-        
-            if (!$Orangtua->anaks->isEmpty()) {
-                foreach ($Orangtua->anaks as $anak) {
-                    // $umuranak = $this->cekumur($anak->tanggal_lahir);
-                    // if ($umuranak > 0 && $umuranak <= 24) {
-                    
-                        if (!$anak->penimbangans->isEmpty()) {
-                            foreach ($anak->penimbangans as $penimbangan) {
-                                // return $penimbangan->orderBy('id','desc')->first();
-
-                                $p = $penimbangan->select(
-                                    DB::raw('count(id) as `jumlah`'),
-                                    )->where('status_bb_pb','Gizi Buruk')->limit(1)
-                                   
-                                    
-                                    // DB::raw("tanggal_kursus","id_jam"),
-                                    // DB::raw("id_jam as jam_kursus" ),)
-                                    // ->groupBy(['tanggal_kursus','id_jam'])->orderBy('tanggal_kursus')
-                                    ->get();
-                                    return $p;
-                              
-                            }
-                        // }
-                    
-                    }
-                }
-            }
-        }
-
-
-
-        
-
-          
-
-
-        }
-        return $data_pos;
-
-    
-        // $ortu = Orangtua::where('posyandu_id',$id)->get();
-        // foreach ($ortu as $key => $value) {
-        //     // $ortu[$key]->jumlah_anak = DB::table('anak')
-        //     // ->where('orangtua_id',$value->id)
-        //     // ->count();
-        //     $anak = Anak::where('orangtua_id',$value->id)->get();
-        //     // $isi_anak = $anak;
-          
-        // }
-        // return $anak;
-
-        // // return $ortu->anak;
-
         // return $data_pos;
 
-        // $ortu = Orangtua::where('posyandu_id', $data_pos->id)->with(
-        //     ['anaks.penimbangans'
-        //     => function ($q)  {
-        //         $q->select("*", DB::raw('YEAR(created_at) year, MONTH(created_at) bulan'));
-        //         // $q->orderBy('id', 'desc')->first();
-              
-              
-        //     }]
-        // )->get();
-
-     
-
-        // return $ortu;
+        
 
         return view('admin.rekap_posyandu', compact('data_pos'));
     }
